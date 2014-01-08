668efd7d2a0c50817508798f223850c33ea3980e
Jay Jacobs <jay.jacobs@verizon.com>
2014-01-08 13:50:59 -0600

    added support.R for supporting functions

---
+++ R/support.R
@@ -0,0 +1,30 @@
+#' convert industry codes to short labels
+#' 
+#' given a vector of 2 or 3 digit industry codes, returns
+#' a character vector of short labels as an ordered factor.
+#' 
+#' @param industry a vector of 2 or 3 digit  verisr object
+#' @export
+industry2label <- function(industry) {
+  data(industry2, envir = environment())
+  data(industry3, envir = environment())
+  ind <- rbind(industry2, industry3)
+  sapply(industry, function(x) {
+    ifelse(x %in% ind$code, as.character(ind$short[which(ind$code==x)]), NA)
+  })
+}
+
+#' convert veris industry field names to short labels
+#' 
+#' given a vector of veris names with industries, 
+#' (victim.industry.44 for example), returns
+#' a character vector of short labels as an ordered factor.
+#' 
+#' @param industry a vector of 2 or 3 digit veris industry names
+#' @export
+indname2label <- function(industry) {
+  codes <- sapply(industry, function(x) {
+    unlist(strsplit(x, '[.]'))[3]
+  })
+  industry2label(codes)
+}
