2be6986589debd62b1c429485c41b7a156099094
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-12-11 09:07:25 -0600

    adds more tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -12,4 +12,24 @@ test_that("a new verisr object has these columns", {
   # Note: not testing every column, just some key ones that have been added
   expect_equal(("victim.industry.name" %in% names(vr)), TRUE)
   expect_equal(("victim.industry2" %in% names(vr)), TRUE)
+  expect_equal(("victim.orgsize.Small" %in% names(vr)), TRUE)
+  expect_equal(("victim.orgsize.Large" %in% names(vr)), TRUE)
+  expect_equal(("victim.industry3" %in% names(vr)), TRUE)
+  expect_equal(("actor.partner.industry2" %in% names(vr)), TRUE)
+})
+
+test_that("a verisr object counts actors properly: getenum(\"actor\"", {
+  # Question: Do we need to test every enumeration that getenum might 
+  # see?
+  actors <- getenum(vr, "actor")
+  expect_equal(actors[actors$enum=="External",]$x, 52)
+  expect_equal(actors[actors$enum=="Internal",]$x, 42)
+  expect_equal(actors[actors$enum=="Partner",]$x, 6)
+  expect_equal(actors[actors$enum=="Unknown",]$x, 1)
+})
+
+test_that("post.proc() properly calculates which records are small and largevictims", {
+  # Manually count how many from our test set are Small and check
+  # that against sum(vr$victim.orgsize.Small)
+  # sum(vr$victim.orgsize.Large)
 })
