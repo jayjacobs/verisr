4d345f0409e5ce513b2364d0a176b2a073277d60
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-12-11 08:44:07 -0600

    tests that industry2 and victim.industry.name are in a new verisr object

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -7,3 +7,9 @@ test_that("a new verisr object has the correct number of records", {
 test_that("a new verisr object has the correct classes", {
   expect_equal(class(vr), c("verisr", "data.table", "data.frame"))
 })
+
+test_that("a new verisr object has these columns", {
+  # Note: not testing every column, just some key ones that have been added
+  expect_equal(("victim.industry.name" %in% names(vr)), TRUE)
+  expect_equal(("victim.industry2" %in% names(vr)), TRUE)
+})
